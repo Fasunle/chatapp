755e51dbe2e2df9f08b9aad0901608eefc80c397
Kehinde Fasunle <kfasunle@gmail.com>
2023-01-04 23:27:20 +0100

    feat: test connection with backend

--- apps/chatapp/src/app/app.tsx
+++ apps/chatapp/src/app/app.tsx
@@ -1,12 +1,17 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.scss';
 import NxWelcome from './nx-welcome';
+import { socket } from './socket';
 
 export function App() {
+  const greet = () => {
+    socket.emit( 'message', "Hello from the client", ( data: any ) => console.log( data ) );
+  }
   return (
     <>
-      <NxWelcome title="chatapp" />
+      {/* <NxWelcome title="chatapp" /> */}
       <div />
+      <h1 onClick={greet}>Hello world</h1>
     </>
   );
 }
